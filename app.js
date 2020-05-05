bf725a23465c64208ffb6b802f1ef81f7c44d821
Claire Vandenberg <vandenberg_claire@hotmail.com>
2020-05-05 11:25:57 +0930

    code now working in terminal

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ const writeFileAsync = util.promisify(fs.writeFile);
 // Write code to use inquirer to gather information about the development team members,
 
 //Calling Functions
-userQuestionsResponse ();
 createEngineer (); 
 createIntern (); 
 createManager ();
@@ -194,13 +193,6 @@ async function createIntern () {
 }
 
 
-
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
 //generate HTML Page through rendering employees.
 const html = render(employees);
 fs.writeFile(outputPath, render(employees), (err) => {
@@ -208,23 +200,3 @@ fs.writeFile(outputPath, render(employees), (err) => {
         throw err;
     }
 });
-
-
-
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
