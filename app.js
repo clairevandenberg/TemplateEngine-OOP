2a2165863bd3731ee482c0aa394378de7c8b4025
Claire Vandenberg <vandenberg_claire@hotmail.com>
2020-05-05 12:45:13 +0930

    debugging & added option to add more employees by calling createEmployee fucntion again ELSE the HTML page will be built

--- app.js
+++ app.js
@@ -103,6 +103,8 @@ function createManager() {
 ]).then(userInput => {
     const manager = new Manager (userInput.ManagerName, userInput.ManagerID , userInput.ManagerEmail, userInput.ManagerOfficeNumber);
     employees.push(manager);
+    console.log("Manager Pushed")
+
 });
 }
 
@@ -160,6 +162,8 @@ function createEngineer () {
 ]).then(userInput => {
     const engineer = new Engineer (userInput.EngineerName, userInput.EngineerID , userInput.EngineerEmail, userInput.EngineerGithubUsername);
     employees.push(engineer);
+    console.log("Engineer Pushed")
+
 });
 }
 
@@ -217,11 +221,11 @@ function createIntern () {
 ]).then(userInput => {
     const intern = new Intern (userInput.InternName, userInput.InternID , userInput.InternEmail, userInput.InternSchool);
     employees.push(intern);
+    console.log("Intern Pushed")
 });
-}
+};
 
-
-//creating Manager, Intern and Engineer  
+//creating Manager, Intern and Engineer >>> employeeBuilder if manager is selected will promt the manager messages.
 async function createEmployee () {
     try { 
     const addingEmployee = await employeeBuilder ()
@@ -237,24 +241,27 @@ if (addingEmployee.employeetype === "Manager") {
 } else if (addingEmployee.employeetype === "Intern") {
     const addingIntern= await createIntern ();
         console.log("Intern has been added to employeee list");
-
 }  
+
+//Do You Want To Add Another Team Member? If YES then will call the createEmployee function again to add more employees. ELSE will build your HTML page. 
+moreEmployees = await addMoreEmployees ()
+if (moreEmployees.addAnotherEmployee === "Yes") {
+    createEmployee ()
+
+} else 
+{
+// generate HTML Page through rendering employees.
+const html = render(employees);
+fs.writeFile(outputPath, render(employees), (err) => {
+    console.log("Your employee list is complete");
+});
+}
 }
 catch (error) {
     console.log(error)
 }
 };
 
-//calling function to create a new employee
-createEmployee ();
 
 
 
-
-//generate HTML Page through rendering employees.
-// const html = render(employees);
-// fs.writeFile(outputPath, render(employees), (err) => {
-//     if (err){
-//         throw err;
-//     }
-// });
